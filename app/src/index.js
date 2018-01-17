e3471c11d97d566dead8f0f9092da1cb9d62b83f
Julio <Julioarhernandez@gmail.com>
2018-01-17 08:29:58 -0500

    added bootstrap to project

--- app/src/index.js
+++ app/src/index.js
@@ -6,6 +6,8 @@ import RouteApp from './components/RouteApp';
 // Styles
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 
