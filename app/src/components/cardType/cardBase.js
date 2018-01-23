e80057d9d5ccf5c3b04a32c487c46afc6ab21a85
Julio <Julioarhernandez@gmail.com>
2018-01-22 23:31:57 -0500

    background loading

--- app/src/components/cardType/cardBase.js
+++ app/src/components/cardType/cardBase.js
@@ -9,7 +9,9 @@ const CardBase = (props) => {
                <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span>
               </div>
               <a href={'/showcard/' + props.cardData._id}>
+              <div className="imgWrap">
                <img src={props.cardData.cardImgSrc} />
+               </div>
               <h2>{props.cardData.cardTitle}</h2></a>
         </div>
     );
