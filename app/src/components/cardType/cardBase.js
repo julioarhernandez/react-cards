952f0a3f737cd9e5cb3e63e01ecdea145b2e550f
Julio <Julioarhernandez@gmail.com>
2018-08-07 00:44:29 -0400

    Card base and coupon changes

--- app/src/components/cardType/cardBase.js
+++ app/src/components/cardType/cardBase.js
@@ -4,17 +4,24 @@ import DropDownMenu from '../dropDownMenu';
 const CardBase = (props) => {
     return (
         <div className="roundCard" key={props.cardData._id}>
-              <div className="bizTitle">
-               <img id="imgletterhead" src={props.bizLogo} alt={props.bizName}/>
+            <a href={'/showcard/' + props.cardData._id}>
+                <div className="imgWrap">
+                    <img src={props.cardData.cardImgSrc} className="Card-detail-link" alt={props.cardData.cardTitle} />
+                </div>
+            </a>
+            <div className="bizTitle">
+               {/* <img id="imgletterhead" src={props.bizLogo} alt={props.bizName}/> */}
                <h2>{props.bizName}</h2>
-               <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span>
+               {/* <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span> */}
                <DropDownMenu web={props.bizWeb} number={props.bizPhone} location={props.bizLocation} />
-              </div>
-              <a href={'/showcard/' + props.cardData._id}>
-              <div className="imgWrap">
-               <img src={props.cardData.cardImgSrc} className="Card-detail-link" alt={props.cardData.cardTitle} />
-               </div>
-              <h2 className="Card-detail-link" >{props.cardData.cardTitle}</h2></a>
+            </div>
+            <a href={'/showcard/' + props.cardData._id}>
+                <h2 className="Card-detail-link" >{props.cardData.cardTitle}</h2>
+            </a>
+            <hr />
+            <a href={'/showcard/' + props.cardData._id} className="materialLink">
+                Explore
+            </a>
         </div>
     );
 };
--- app/src/components/cardType/cardBaseCoupon.js
+++ app/src/components/cardType/cardBaseCoupon.js
@@ -4,17 +4,24 @@ import DropDownMenu from '../dropDownMenu';
 const CardBaseCoupon = (props) => {
     return (
         <div className="roundCard" key={props.cardData._id}>
-              <div className="bizTitle">
-               <img id="imgletterhead" src={props.bizLogo} alt={props.bizName}/>
+            <a href={'/showcard/' + props.cardData._id}>
+                <div className="imgWrap">
+                    <img src={props.cardData.cardImgSrc} className="Card-detail-link" alt={props.cardData.cardTitle} />
+                </div>
+            </a>
+            <div className="bizTitle">
+               {/* <img id="imgletterhead" src={props.bizLogo} alt={props.bizName}/> */}
                <h2>{props.bizName}</h2>
-               <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span>
+               {/* <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span> */}
                <DropDownMenu web={props.bizWeb} number={props.bizPhone} location={props.bizLocation} />
-              </div>
-              <a className="Card-detail-link" href={'/showcardcoupon/' + props.cardData._id}>
-              <div className="imgWrap">
-               <img src={props.cardData.cardImgSrc} className="Card-detail-link" alt={props.cardData.cardTitle}/>
-               </div>
-              <h2 className="Card-detail-link" >{props.cardData.cardTitle}</h2></a>
+            </div>
+            <a href={'/showcardcoupon/' + props.cardData._id}>
+                <h2 className="Card-detail-link" >{props.cardData.cardTitle}</h2>
+            </a>
+            <hr />
+            <a href={'/showcard/' + props.cardData._id} className="materialLink">
+                Explore
+            </a>
         </div>
     );
 };
