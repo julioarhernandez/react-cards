09196aa403f9cd7fbc91d160e44d0bdf42f9b8d5
Julio <Julioarhernandez@gmail.com>
2018-08-07 00:43:46 -0400

    Footer less links

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
         <div className="banner-footer">
         {/* <img src="../logo-white.svg" /> */}
         <ul>
-            <li><a href="https://bizmarketing.us/en/contactanos-campana-publicitaria/" target="_blank" rel="noopener noreferrer" >Support</a></li>
             <li><a href="http://bizmarketing.us/politica-terminos/" target="_blank" rel="noopener noreferrer">Terms of use</a></li>
             <li><a href="https://bizmarketing.us/en/privacy-policy-dealby/" target="_blank" rel="noopener noreferrer">Privacy policy</a></li>
             <li><a href="https://bizmarketing.us/en/dealby-mejores-ofertas/" target="_blank" rel="noopener noreferrer" >know more</a></li>
