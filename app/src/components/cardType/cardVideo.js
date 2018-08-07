a92329a69a8f1f2324cc8e747578d544774a11cd
Julio <Julioarhernandez@gmail.com>
2018-08-07 00:43:26 -0400

    Video card

--- app/src/components/cardType/cardVideo.js
+++ app/src/components/cardType/cardVideo.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import '../../topDownVideo.css';
+import DropDownMenu from '../dropDownMenu';
 
 const CardVideo = (props) => {
     return (
         <div className="roundCard fullImage fullVideo" key={props.cardData._id}>
-              <div className="bizTitle">
-               <img id="imgletterhead" src={props.bizLogo} alt={props.bizName}/>
-               <h2>{props.bizName}</h2>
-               <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span>
-              </div>
-              <a href={props.cardData.cardLink} target="_blank">
-               <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
+            <a href={props.cardData.cardLink} target="_blank">
+                <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
                <div className="shadow">
-               <div className="imgWrap">
-                 <img src={props.cardData.cardImgSrc} className="Card-detail-link" alt={props.cardData.cardTitle}/>
-               </div>
-               </div>
-               <h2 className="Card-detail-link" >{props.cardData.cardTitle}</h2>
-              </a>
+                <div className="imgWrap">
+                    <img src={props.cardData.cardImgSrc} className="Card-detail-link" alt={props.cardData.cardTitle} />
+                </div>
+                </div>
+            </a>
+            <div className="bizTitle">
+               {/* <img id="imgletterhead" src={props.bizLogo} alt={props.bizName}/> */}
+               <h2>{props.bizName}</h2>
+               {/* <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span> */}
+               <DropDownMenu web={props.bizWeb} number={props.bizPhone} location={props.bizLocation} />
+            </div>
+            <a href={props.cardData.cardLink} target="_blank">
+                <h2 className="Card-detail-link" >{props.cardData.cardTitle}</h2>
+            </a>
+            <hr />
+            <a href={props.cardData.cardLink} className="materialLink" target="_blank">
+                Open Video
+            </a>
         </div>
+
+        // <div className="roundCard fullImage fullVideo" key={props.cardData._id}>
+        //       <div className="bizTitle">
+        //        <img id="imgletterhead" src={props.bizLogo} alt={props.bizName}/>
+        //        <h2>{props.bizName}</h2>
+        //        <span>{props.bizAddress.street}. {props.bizAddress.county}, {props.bizAddress.state} {props.bizAddress.zip}</span>
+        //       </div>
+        //       <a href={props.cardData.cardLink} target="_blank">
+        //        <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
+        //        <div className="shadow">
+        //        <div className="imgWrap">
+        //          <img src={props.cardData.cardImgSrc} className="Card-detail-link" alt={props.cardData.cardTitle}/>
+        //        </div>
+        //        </div>
+        //        <h2 className="Card-detail-link" >{props.cardData.cardTitle}</h2>
+        //       </a>
+        // </div>
     );
 };
 export default CardVideo;
