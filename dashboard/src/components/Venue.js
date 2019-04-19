8aa43933d371482f977a04bbab98c95791e2ac4f
Julio <Julioarhernandez@gmail.com>
2019-04-19 00:26:48 -0400

    Venue component updated

--- dashboard/src/components/Venue.js
+++ dashboard/src/components/Venue.js
@@ -15,6 +15,7 @@ class Venue extends Component {
     
         this.state = {
             showAlerts: false,
+            message: '',
             email: '',
             password: ''
           };
@@ -22,10 +23,19 @@ class Venue extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
         this.preventSubmit = this.preventSubmit.bind(this);
+        this.validateInputs = this.validateInputs.bind(this);
 
     }
     preventSubmit(e){
         e.preventDefault();
+    }
+    validateInputs(){
+        return  this.state.name && 
+                this.state.slug && 
+                this.state.polygonLocation &&
+                this.state.pointLocation &&
+                this.state.addressStreet &&
+                this.state.addressZip
     }
     handleChange(e){
         this.setState(
@@ -37,35 +47,40 @@ class Venue extends Component {
     }
     handleFormSubmit(e){
         e.preventDefault();
-        // const formData = new FormData();
-        // formData.append('email', this.state.email);
-        // formData.append('password', this.state.password);
-        this.Auth.fetch(`${baseUrl}/api/cards/addvenue/`,{
-            method: 'POST',
-            body: JSON.stringify({
-                veName: this.state.name, 
-                veSlug: this.state.slug,
-                veCoordinates: this.state.polygonLocation,
-                vePointLocation: this.state.pointLocation,
-                veAddressCountry: this.state.addressCountry,
-                veAddressState: this.state.addressState,
-                veAddressStreet: this.state.addressStreet,
-                veAddressZip: this.state.addressZip,
-                veAddressCounty: this.state.addressCounty
+        if (this.validateInputs()){
+            this.Auth.fetch(`${baseUrl}/api/cards/addvenue/`,{
+                method: 'POST',
+                body: JSON.stringify({
+                    veName: this.state.name, 
+                    veSlug: this.state.slug,
+                    veCoordinates: this.state.polygonLocation,
+                    vePointLocation: this.state.pointLocation,
+                    veAddressCountry: this.state.addressCountry,
+                    veAddressState: this.state.addressState,
+                    veAddressStreet: this.state.addressStreet,
+                    veAddressZip: this.state.addressZip,
+                    veAddressCounty: this.state.addressCounty
+                })
+            }).then(response => {
+                if (response.status === 'Success'){
+                    this.setState({ 
+                        showAlerts: true,
+                        message: 'Changes saved sucessfully!'
+                    });
+                }else {
+                    console.log(response);
+                    this.setState({ 
+                        showAlerts: true,
+                        message: 'Error. Please try again!'
+                    });
+                }
+            });
+        }else{
+            this.setState({ 
+                showAlerts: true,
+                message: 'Fill required inputs'
             })
-        }).then(response => {
-            if (response.status === 'Success'){
-                this.setState({ 
-                    showAlerts: true,
-                    message: 'Changes saved sucessfully!'
-                });
-            }else {
-                this.setState({ 
-                    showAlerts: true,
-                    message: 'Error. Please try again!'
-                });
-            }
-        });
+        }
     }
 
   render() {
@@ -89,20 +104,13 @@ class Venue extends Component {
                             </div>
                             <div className="VenueCard-pointLocation">
                                 <label htmlFor="pointLocation">Point Location</label>
-                                <input type="text" name="pointLocation" id="pointLocation" onChange={this.handleChange} value={this.state.pointLocation} />
+                                <input type="text" name="pointLocation" id="pointLocation" onChange={this.handleChange} value={this.state.pointLocation} placeholder="lat,long"/>
                             </div>
                             <div className="VenueCard-polygonLocation">
                                 <label htmlFor="polygonLocation">Boundary coordinates</label>
-                                <textarea rows="4" value={this.state.polygonLocation} name="polygonLocation" id="polygonLocation" onChange={this.handleChange}/>
+                                <textarea rows="4" value={this.state.polygonLocation} name="polygonLocation" id="polygonLocation" onChange={this.handleChange} placeholder="lat,long;lat,long;...lat,long"/>
                             </div>
-                            <div className="VenueCard-address">
-                                <label htmlFor="addressCountry">Country</label>
-                                <input type="text" name="addressCountry" id="addressCountry" onChange={this.handleChange} value={this.state.addressCountry} />
-                            </div>
-                            <div className="VenueCard-address">
-                                <label htmlFor="addressState">State</label>
-                                <input type="text" name="addressState" id="addressState" onChange={this.handleChange} value={this.state.addressState} />
-                            </div>
+
                             <div className="VenueCard-address">
                                 <label htmlFor="addressStreet">Street</label>
                                 <input type="text" name="addressStreet" id="addressStreet" onChange={this.handleChange} value={this.state.addressStreet} />
@@ -113,9 +121,16 @@ class Venue extends Component {
                             </div>
                             <div className="VenueCard-address">
                                 <label htmlFor="addressCity">City</label>
-                                <input type="text" name="addressCity" id="addressCity" onChange={this.handleChange} value={this.state.addressCity} />
+                                <input type="text" name="addressCity" id="addressCity" onChange={this.handleChange} value={this.state.addressCity} placeholder="Default:Miami"/>
                             </div>
-                            
+                            <div className="VenueCard-address">
+                                <label htmlFor="addressState">State</label>
+                                <input type="text" name="addressState" id="addressState" onChange={this.handleChange} value={this.state.addressState} placeholder="Default:FL"/>
+                            </div>
+                            <div className="VenueCard-address">
+                                <label htmlFor="addressCountry">Country</label>
+                                <input type="text" name="addressCountry" id="addressCountry" onChange={this.handleChange} value={this.state.addressCountry} placeholder="Default:US"/>
+                            </div>
                         </div>
                         <div className="VenueCard-aside">
                             <Link to="#" className="btn btn-blue -block" onClick={this.handleFormSubmit}>
